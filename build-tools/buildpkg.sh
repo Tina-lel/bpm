4282986275d635dac6ff4302e79cdf5181ad7816
Justus0405 <justus040506@gmail.com>
2024-12-06 19:05:10 +0100

    Formatting and improved error handling

--- build-tools/buildpkg.sh
+++ build-tools/buildpkg.sh
@@ -2,13 +2,14 @@
 
 # Simple tool to build packages for the bpm package manager
 
-if [[ $1 == "ALL" ]]
-then
+if [[ $1 == "ALL" ]]; then
     echo -e "Building all packages\n"
     mkdir -p "pkgs"
-    cd "src_pkgs"
-    for i in *
-    do
+    cd "src_pkgs" || {
+        echo "Error: could not find src_pkgs folder"
+        exit 1
+    }
+    for i in *; do
         echo -e "Building $i"
         mkdir -p "$i/src"
         mkdir -p "$i/build"
@@ -17,20 +18,17 @@ then
     exit 0
 fi
 
-if [[ "$1" == "" ]]
-then
+if [[ "$1" == "" ]]; then
     echo -e "Requires name"
     exit 1
 fi
 
-if [[ ! -d "src_pkgs/$1" ]]
-then
+if [[ ! -d "src_pkgs/$1" ]]; then
     echo -e "No such folder name in src_pkgs/"
     exit 1
 fi
 
-if [[ ! -f "src_pkgs/$1/pkg_info" ]]
-then
+if [[ ! -f "src_pkgs/$1/pkg_info" ]]; then
     echo -e "Not a valid packet (missing pkg_info file)"
     exit 1
 fi
@@ -39,5 +37,8 @@ echo -e "Building $1"
 mkdir -p "pkgs"
 mkdir -p "src_pkgs/$1/src"
 mkdir -p "src_pkgs/$1/build"
-cd "src_pkgs"
+cd "src_pkgs" || {
+    echo "Error: could not find src_pkgs folder"
+    exit 1
+}
 tar -czf "../pkgs/$1.tar.gz" "$1"
