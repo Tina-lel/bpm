6deb4d655ea212b40bb85130ed2b360ffbebd85a
Tina-lel <saman107@protonmail.com>
2024-12-06 11:14:39 +0100

    added build tools for creation of packages for this shitty package manager

---
+++ build-tools/buildpkg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Simple tool to build packages for the bpm package manager
+
+if [[ "$1" == "" ]]
+then
+    echo -e "Requires name"
+    exit 1
+fi
+
+if [[ ! -d "src_pkgs/$1" ]]
+then
+    echo -e "No such folder name in src_pkgs/"
+    exit 1
+fi
+
+if [[ ! -f "src_pkgs/$1/pkg_info" ]]
+then
+    echo -e "Not a valid packet (missing pkg_info file)"
+    exit 1
+fi
+
+echo -e "Creating package"
+tar -czf "pkgs/$1.tar.gz" "src_pkgs/$1"
