f33b723d27ca6124d8427722d38b22eca32d19c8
Tina-lel <saman107@protonmail.com>
2024-12-10 15:12:25 +0100

    removed purge because it was being a meanie >:(

--- build-tools/buildpkg.sh
+++ build-tools/buildpkg.sh
@@ -31,7 +31,6 @@ case "$1" in
         echo "DOWNLOAD=\"$DOWNLOAD\"" >> "../pkgs/info/$i.info"
         echo "COMPILE=\"$COMPILE\"" >> "../pkgs/info/$i.info"
         echo "INSTALLED_FILES=(${INSTALLED_FILES[*]})" >> "../pkgs/info/$i.info"
-        echo "CONFIG_FILES=(${CONFIG_FILES[*]})" >> "../pkgs/info/$i.info"
 
         md5=($(md5sum "../pkgs/$SORT/$i.tar.gz"))
         echo "$md5  $i.tar.gz" >> "../pkgs/info/$i.info"
@@ -85,7 +84,6 @@ echo "DEPENDENCIES=\"$DEPENDENCIES\"" >> "../pkgs/info/$1.info"
 echo "DOWNLOAD=\"$DOWNLOAD\"" >> "../pkgs/info/$1.info"
 echo "COMPILE=\"$COMPILE\"" >> "../pkgs/info/$1.info"
 echo "INSTALLED_FILES=(${INSTALLED_FILES[@]})" >> "../pkgs/info/$1.info"
-echo "CONFIG_FILES=(${CONFIG_FILES[*]})" >> "../pkgs/info/$1.info"
 
 md5=($(md5sum "../pkgs/$SORT/$1.tar.gz"))
 echo "$md5  $1.tar.gz" >> "../pkgs/info/$1.info"
